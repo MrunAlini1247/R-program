3af1a0bebc154f1c79bf43f527449663631696ac
Mrunalini Dhoble <77576253+MrunAlini1247@users.noreply.github.com>
2021-01-17 20:02:41 +0530

    Add files via upload

---
+++ rprogram1.R
@@ -0,0 +1,10 @@
+#introduction
+print("welcome to analytics edge class")
+a<-("welcome to anylystics edge class")
+#a<-(print("welcome to analystics edge class")
+a
+
+#plotting
+X=1:5
+Y=6:10
+plot(X,Y)
